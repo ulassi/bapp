c05c05c16f1a99c73717862a5049041d1d32d854
ulassi <ulassi@gmail.com>
2015-09-06 14:36:45 +0200

    build debug information

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 g++ -v
-g++ -Wall -I ./ -c app.cpp -o build/app.o
-g++ -Wall -I ./ -framework Foundation -lObjC window.mm -o build/app build/app.o 
+g++ -g -Wall -Werror -I ./ -c app.cpp -o build/app.o
+g++ -g -Wall -Werror -I ./ -I /System/Library/Frameworks/OpenGL.framework/Headers -framework Cocoa -framework OpenGL -lObjC window.mm -o build/app build/app.o 
